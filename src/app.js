4880be096ca3f6b4c48816e1ce274a27b435a8aa
lk3v1nn <kevinlmorataya@gmail.com>
2023-10-15 21:01:53 -0600

    configuracion de headers de login y axios

--- src/app.js
+++ src/app.js
@@ -4,12 +4,19 @@ const usuariosRouter = require('./controllers/routes/usuarios')
 const reservacionesRouter = require('./controllers/routes/reservasiones')
 const app = Express();  
 
+app.use(function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, x-access-token');
+  next();
+});
+
 app.use(Express.json());
 app.use(usuariosRouter);
 app.use(reservacionesRouter);
 
 app.get('/', (req, res) =>{
-    res.send('hola')
+    res.json({"mensaje": "hola"})
 })
 
 app.listen('3000', () => {
